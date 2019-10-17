39bdef440a8ee0ccecb14fded0d8bdd1e9d6a3e8
Gustavo Amaral <gamaralm@hotmail.com>
2019-10-17 15:48:56 -0300

    chore: Configurações do Banco de Dados, Migration e Seed iniciais

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3307,
+  username: 'root',
+  password: 'docker',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
